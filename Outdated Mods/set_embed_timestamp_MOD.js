b76b8a970fef891804cebb1934e64c4be0f0a081
ACertainCoder <47316898+ACertainCoder@users.noreply.github.com>
2020-02-29 11:53:14 +0100

    Updated author name

--- Outdated Mods/set_embed_timestamp_MOD.js
+++ Outdated Mods/set_embed_timestamp_MOD.js
@@ -47,7 +47,7 @@ subtitle: function(data) {
 	 //---------------------------------------------------------------------
 
 	 // Who made the mod (If not set, defaults to "DBM Mods")
-	 author: "ZockerNico",
+	 author: "ACertainCoder",
 
 	 // The version of the mod (Defaults to 1.0.0)
 	 version: "1.9.5",//Added in 1.9.5
